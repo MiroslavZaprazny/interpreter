c2eff4cffc6d484b34cb85bb4455244bc43414d1
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-08-24 09:23:05 +0200

    evaluate in repl

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"interpreter/evaluator"
 	"interpreter/lexer"
 	"interpreter/parser"
 	"io"
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
             printParserErrors(out, p.Errors())
         }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, string(evaluated.Inspect()))
+            io.WriteString(out, "\n")
+        }
     }
 }
 
