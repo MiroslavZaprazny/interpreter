068d5d73e9725b3191c14b23d760473a30f1ac34
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-08-24 11:29:21 +0200

    eval prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -8,6 +8,7 @@ import (
 var (
     TRUE = &object.Boolean{Value: true}
     FALSE = &object.Boolean{Value: false}
+    NULL = &object.Null{}
 )
 
 func Eval(node ast.Node) object.Object {
@@ -16,10 +17,13 @@ func Eval(node ast.Node) object.Object {
             return evalStatements(node.Statements)
         case *ast.ExpressionStatement:
             return Eval(node.Expression)
+        case *ast.PrefixExpression:
+            right := Eval(node.Right)
+            return evalPrefixExpression(node.Operator, right)
         case *ast.IntegerLiteral:
             return &object.Integer{Value: node.Value}
         case *ast.Boolean:
-            return &object.Boolean{Value: node.Value}
+            return nativeBoolToBooleanObject(node.Value)
     }
 
     return nil
@@ -41,4 +45,38 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
     }
 
     return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+        case "!":
+            return evalBangOperatorExpression(right)
+        case "-":
+            return evalMinuxPrefixOperatorExpression(right)
+        default:
+            return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+        case TRUE:
+            return FALSE
+        case FALSE:
+            return TRUE
+        case NULL:
+            return TRUE
+        default:
+            return FALSE
+    }
+}
+
+func evalMinuxPrefixOperatorExpression(right object.Object) object.Object {
+   if right.Type() != object.INTEGER_OBJ {
+      return NULL
+   }
+
+   value := right.(*object.Integer).Value
+
+   return &object.Integer{Value: -value}
 }
